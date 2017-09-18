7a3de744e839b379ddea779c5b3820ed64c60981
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-18 05:45:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var a = 10
+var b = 15
+var n = 3
+
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
